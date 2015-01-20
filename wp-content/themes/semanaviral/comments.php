ee4b43d106f316c95e788e72f864b7b16a90292b
jesser pariona <jerspr@gmail.com>
2015-01-19 23:36:50 -0500

    üpdate

--- wp-content/themes/semanaviral/comments.php
+++ wp-content/themes/semanaviral/comments.php
@@ -1,76 +1,79 @@
 <?php
 //removing the comments sidewide
 if(td_util::get_option('tds_disable_comments_sidewide') == '') {?>
-                        <div class="comments" id="comments">
-                            <?php if (post_password_required()) { ?>
+    <div class="comments" id="comments">
+        <?php if (post_password_required()) { ?>
+        <?php } else { ?>
+            <?php if (comments_open() ) { ?>
+                <div class="comments-title-wrap">
+                    <?php
+                    $num_comments = get_comments_number(); // get_comments_number returns only a numeric value
+                    if ( $num_comments == 0 ) {
+                        $td_comments_no_text = __td('NO COMMENTS');
+                    } elseif ( $num_comments > 1 ) {
+                        $td_comments_no_text = $num_comments . ' ' . __td('COMMENTS');
+                    } else {
+                        $td_comments_no_text = __td('1 COMMENT');
+                    }
+                    ?>
+                    <h4 class="block-title"><span><?php echo $td_comments_no_text?></span></h4>
+                </div>
 
-                            <?php } else { ?>
-                                <?php if (comments_open() ) { ?>
-                                    <div class="comments-title-wrap">
-                                        <?php
-                                        $num_comments = get_comments_number(); // get_comments_number returns only a numeric value
-                                        if ( $num_comments == 0 ) {
-                                            $td_comments_no_text = __td('NO COMMENTS');
-                                        } elseif ( $num_comments > 1 ) {
-                                            $td_comments_no_text = $num_comments . ' ' . __td('COMMENTS');
-                                        } else {
-                                            $td_comments_no_text = __td('1 COMMENT');
-                                        }
-                                        ?>
-                                        <h4 class="block-title"><span><?php echo $td_comments_no_text?></span></h4>
-                                    </div>
+                <?php if (have_comments()) { ?>
+                    <ol class="comment-list">
+                        <?php wp_list_comments( array( 'callback' => 'td_comment' ) ); ?>
+                    </ol>
+                    <div class="comment-pagination">
+                        <?php paginate_comments_links(); ?>
+                    </div>
+                <?php } ?>
+        <?php } ?>
+        <?php
 
-                                    <?php if (have_comments()) { ?>
-                                        <ol class="comment-list">
-                                            <?php wp_list_comments( array( 'callback' => 'td_comment' ) ); ?>
-                                        </ol>
-                                        <div class="comment-pagination">
-                                            <?php paginate_comments_links(); ?>
-                                        </div>
-                                    <?php } ?>
-                            <?php } ?>
-                            <?php
+        $commenter = wp_get_current_commenter();
 
-                            $commenter = wp_get_current_commenter();
+        if (empty($aria_req)) {
+            $aria_req = '';
+        }
+        $fields =  array(
+            'author' =>
+            '<p class="comment-form-input-wrap">
+                <span class="comment-req-wrap needsclick"><input class="" id="author" name="author" placeholder="' . __td('Name:', TD_THEME_NAME) . '" type="text" value="' . esc_attr( $commenter['comment_author'] ) . '" size="30"' . $aria_req . ' />' . ( $req ? '</span>' : '' ) .
+            '</p>',
 
-                            if (empty($aria_req)) {
-                                $aria_req = '';
-                            }
-    $fields =  array(
-        'author' =>
-        '<p class="comment-form-input-wrap">
-            <span class="comment-req-wrap needsclick"><input class="" id="author" name="author" placeholder="' . __td('Name:', TD_THEME_NAME) . '" type="text" value="' . esc_attr( $commenter['comment_author'] ) . '" size="30"' . $aria_req . ' />' . ( $req ? '</span>' : '' ) .
-        '</p>',
+            'email'  =>
+            '<p class="comment-form-input-wrap">
+                <span class="comment-req-wrap needsclick"><input class="" id="email" name="email" placeholder="' . __td('Email:', TD_THEME_NAME) . '" type="text" value="' . esc_attr(  $commenter['comment_author_email'] ) . '" size="30"' . $aria_req . ' />' . ( $req ? '</span>' : '' ) .
+            '</p>',
 
-        'email'  =>
-        '<p class="comment-form-input-wrap">
-            <span class="comment-req-wrap needsclick"><input class="" id="email" name="email" placeholder="' . __td('Email:', TD_THEME_NAME) . '" type="text" value="' . esc_attr(  $commenter['comment_author_email'] ) . '" size="30"' . $aria_req . ' />' . ( $req ? '</span>' : '' ) .
-        '</p>',
+            'url' =>
+            '<p class="comment-form-input-wrap needsclick">
+                <input class="" id="url" name="url" placeholder="' . __td('Website:', TD_THEME_NAME) . '" type="text" value="' . esc_attr( $commenter['comment_author_url'] ) . '" size="30" />' .
+            '</p>',
+        );
 
-        'url' =>
-        '<p class="comment-form-input-wrap needsclick">
-            <input class="" id="url" name="url" placeholder="' . __td('Website:', TD_THEME_NAME) . '" type="text" value="' . esc_attr( $commenter['comment_author_url'] ) . '" size="30" />' .
-        '</p>',
-    );
+        $defaults = array(
+         'fields' => apply_filters('comment_form_default_fields', $fields ),
+        );
 
-    $defaults = array(
-     'fields' => apply_filters('comment_form_default_fields', $fields ),
-    );
+        $defaults['comment_field'] = '<div class="clearfix"></div><p class="comment-form-input-wrap"><textarea class="needsclick" placeholder="' . __td('Comment:', TD_THEME_NAME) . '" id="comment" name="comment" cols="45" rows="8" aria-required="true"></textarea></p>';
 
-    $defaults['comment_field'] = '<div class="clearfix"></div><p class="comment-form-input-wrap"><textarea class="needsclick" placeholder="' . __td('Comment:', TD_THEME_NAME) . '" id="comment" name="comment" cols="45" rows="8" aria-required="true"></textarea></p>';
+        $defaults['comment_notes_before'] = '';
+        $defaults['comment_notes_after'] = '';
+        $defaults['title_reply'] = __td('Leave a Reply');
+        $defaults['label_submit'] = __td('Post Comment');
+        $defaults['cancel_reply_link'] = __td('Cancel reply');
 
-    $defaults['comment_notes_before'] = '';
-    $defaults['comment_notes_after'] = '';
-    $defaults['title_reply'] = __td('Leave a Reply');
-    $defaults['label_submit'] = __td('Post Comment');
-    $defaults['cancel_reply_link'] = __td('Cancel reply');
+        //comment_form($defaults);
+        //comment_form();
 
-    comment_form($defaults);
-                                //comment_form();
+        ?>
+            <div class="fb-comments" data-href="http://developers.facebook.com/docs/plugins/comments/" data-width="100%" data-numposts="10" data-colorscheme="light"></div>
+        <?php
 
-                                ?>
-                            <?php } ?>
-                        </div> <!-- /.content -->
+        ?>
+    <?php } ?>
+    </div> <!-- /.content -->
 
 
 <?php
